59bec0f05c571b5b1f4ccb0761ca72da72ae05df
Christopher Tanudjaja <134334523+Tofusama1@users.noreply.github.com>
2025-02-01 22:54:06 +0000

    I have access my phone camera

--- test.py
+++ test.py
@@ -9,7 +9,14 @@ import open3d as o3d
 
 # # Install Python dependencies
 # pip install opencv-python open3d flask
-
+cap = cv2.VideoCapture(1)
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './uploads'
